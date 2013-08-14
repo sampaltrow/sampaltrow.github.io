b76260ad666e67a11071952aa9cbe9f13d4c0ade
Sam Paltrow <sampaltrow@gmail.com>
2013-08-14 16:47:12 -0400

    changed jquery to mouseenter and mouseleave

--- signals/application.js
+++ signals/application.js
@@ -1,6 +1,17 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
 $(document).ready(function() {
-	 $("#first").on("mouseenter", function() {
-	 	$(this).animate("fast");
-	 	$(this).toggleClass(‘highlighted”);
+	 $(".product-bc").on("mouseenter", function() {
+	 $(this).find(<img src=​"/​images/​arrow-left.png" width=​"69" height=​"69" class=​"arrow">​)
+	 });
+	  $(".product-bc").on("mouseleave", function() {
+	 $(this).removeClass("arrow");	
+	});
+});
+
+
+
+/*
+$(this).animate("fast");
+$(this).toggleClass(‘highlighted”);
+*/
